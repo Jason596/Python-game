5d7f6bb02a83b462c5f9239951827671ad1ccd50
Jason Wu <zwu047@hotmail.com>
2021-03-18 20:46:46 +1300

    bug, when click screen, game stop executing.

--- Game.py
+++ Game.py
@@ -1,14 +1,19 @@
 import pygame
 
-# 1 create a window
-screen = pygame.display.set_mode((602, 967), 0, 32)
+def main():
+    # 1 create a window
+    screen = pygame.display.set_mode((602, 967), 0, 32)
 
-# 2 load background image
-background = pygame.image.load("./background.png")
+    # 2 load background image
+    background = pygame.image.load("./background.png")
 
-# 3 load background image to the window
-screen.blit(background, (0, 0))
+    # 3 load background image to the window
+    screen.blit(background, (0, 0))
 
-# 4 display the window
-pygame.display.update()
+    while True:
+        # 4 display the window
+        pygame.display.update()
 
+
+if __name__ == '__main__':
+    main()
