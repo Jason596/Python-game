a10f7ca3717f016e33947624abbde4ab64c7e167
Jason Wu <zwu047@hotmail.com>
2021-03-18 21:14:10 +1300

    add keyboard movement

--- Game.py
+++ Game.py
@@ -1,4 +1,5 @@
 import pygame
+from pygame import *
 
 
 def main():
@@ -7,18 +8,26 @@ def main():
 
     # 2 load background image
     background = pygame.image.load("./background.png")
+    player = pygame.image.load("./airplane.jpeg")
 
     # 3 load background image to the window
     screen.blit(background, (0, 0))
+    screen.blit(player, (602/2 - 225/2, 700))
 
     while True:
         # events
         for event in pygame.event.get():
-            if event.type == pygame.QUIT:
+            if event.type == QUIT:
                 # exit pygame and exit python program
                 pygame.quit()
                 exit()
-
+            elif event.type == pygame.KEYDOWN:
+                if event.key == K_a or event.key == K_LEFT:
+                    print("left")
+                elif event.key == K_d or event.key == K_RIGHT:
+                    print("right")
+                elif event.key == K_SPACE:
+                    print("space")
         # 4 display the window
         pygame.display.update()
 
